a1875bf7583e836baa2965762dcadb76aa200cc1
yuya-konkon <yuuuuya08@icloud.com>
2020-02-10 16:32:04 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+// 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
+
+
+
+
+
+// 以下を実行した時にエラーがでないようにして下さい。
+// Carクラスのインスタンスを生成
+// 引数 : 車名、車体番号、カラー
+$car1 = new Car('ランクル', 100, 'black');
+
+// $car1->selfIntrodaction();
+// 車名(車種)を取得
+echo $car1->getName();
+echo '<br>';
+
+// 車体番号を取得
+echo $car1->getNumber();
+echo '<br>';
+
+// カラーを取得
+echo $car1->getColor();
+echo '<br>';
+
+// 車の情報表示を表示
+echo $car1->infomation();
+echo '<br>';
+
+// 車名(車種)を更新
+$car1->setName('アルファード');
+echo $car1->getName();
+echo '<br>';
+
+// 車体番号を取得
+$car1->setNumber(200);
+echo $car1->getNumber();
+echo '<br>';
+
+// カラーを取得
+$car1->setColor('red');
+echo $car1->getColor();
+echo '<br>';
+
+// 車の情報表示を表示
+echo $car1->infomation();
+echo '<br>';
