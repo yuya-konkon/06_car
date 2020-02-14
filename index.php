a107549bb6c66ad301c67b67980a741c0f14cd24
yuya-konkon <yuuuuya08@icloud.com>
2020-02-15 00:10:10 +0900

    add taxiClass

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-// 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
 class Car
 {
   private $name;
@@ -50,43 +49,71 @@ class Car
   }
 }
 
-// 以下を実行した時にエラーがでないようにして下さい。
+// 以下にCarクラスを継承したTaxiクラスを記述して下さい
+
+
+// 以下を実行した時にエラーがでないようにして下さい
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// $car1->selfIntrodaction();
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+class Taxi extends Car {
+  private $passenger;
+
+  public function setPassenger($passenger)
+  {
+    $this->passenger = $passenger;
+  }
+  public function getPassenger()
+  {
+    return $this->passenger;
+  }
+
+  public function setPickup($Pickup)
+  {
+    $this->Pickup = $Pickup;
+  }
+  public function Pickup()
+  {
+    return $this->Pickup;
+  }
+
+  public function setLower($lower)
+  {
+    $this->lower = $lower;
+  }
+  public function getLower()
+  {
+    return $this->lower;
+  }
+
+  public function infomation()
+  {
+    parent::infomation();
+    echo '、乗車人数は' . $this->pickUp . '人です。' . '<br>' . $this->lower . '人降車しました';
+  }
+}
+
+
+
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+$taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
-echo '<br>';
+// 2人降車
+// $taxi1->lower(2);
+// echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// // 一人降車
+// $taxi1->lower(2);
