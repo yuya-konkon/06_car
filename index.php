f9cdf768f100b853b7ceaaca8ee657be89720d98
yuya-konkon <yuuuuya08@icloud.com>
2020-02-17 10:01:31 +0900

    add pickup

--- index.php
+++ index.php
@@ -65,25 +65,28 @@ echo '<hr>';
 class Taxi extends Car {
   private $passenger;
 
-  // public function getPickUp()
-  // {
-  //   return $this->PickUp;
-  // }
+  public function __construct($passenger) {
+    $this->passenger = $passenger;
+  }
 
-  // public function setPickUp($PickUp)
-  // {
-  //   $this->color = $PickUp;
-  // }
+  public function setPickUp($PickUp)
+  {
+    $this->PickUp = $PickUp;
+  }
+  public function getPickUp()
+  {
+    return $this->PickUp;
+  }
 
-  // public function getLower()
-  // {
-  //   return $this->Lower;
-  // }
+  public function setLower($Lower)
+  {
+    $this->Lower = $Lower;
+  }
 
-  // public function setLower($Lower)
-  // {
-  //   $this->color = $Lower;
-  // }
+  public function getLower()
+  {
+    return $this->Lower;
+  }
 
   public function setPassenger($passenger)
   {
@@ -96,7 +99,7 @@ class Taxi extends Car {
 
   public function infomation()
   {
-    echo '車の種類:' . $this->getName() . '、' . '車体番号:' . $this->getNumber() . '、' . 'カラー:' . $this->getColor() . '、乗車人数は' . $this->getPickUp . '人です。';
+    echo '車の種類:' . $this->getName() . '、' . '車体番号:' . $this->getNumber() . '、' . 'カラー:' . $this->getColor() . '、乗車人数は' . $this->getPickUp() . '人です。<br>';
   }
 }
 
@@ -106,15 +109,22 @@ class Taxi extends Car {
 $taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
 
 // 3人乗車
-// $taxi1->pickUp(3);
+$taxi1->setpickUp(3);
 
 // タクシーの情報表示を表示
 $taxi1->infomation();
 echo '<br>';
 
 // 2人降車
-// $taxi1->lower(2);
+$taxi1->setlower(2);
 // echo '<br>';
 
 // // // 一人降車
-// $taxi1->lower(2);
+$taxi1->setlower(2);
+
+if ($passenger > 0) {
+  $PickUp - $Lower;
+  echo $Lower . '人降車しました。';
+} else {
+  echo '降車人数に誤りがあります。';
+}
